84292c51da3a4283f7a5e415c746c6f8a07b73b2
dondon <qwerqw889@ajou.ac.kr>
2021-01-28 16:37:53 +0900

    update react movie app tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
+import axios from "axios"
+import Movie from "./Movie"
+import "./App.css"
+// function component랑은 조금 다른 개념
+class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // 변하는 데이터들이 저장될 객체 => state
+  state = {
+    // 반드시 사용한 state 들을 미리 선언할 필요는 없다!
+    isLoading: true,
+    moives: [],
+  };
+
+  getMovies = async() =>{
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    this.setState({movies, isLoading: false});
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
+  // 화면에 출력하는 함수
+  render(){
+    const {isLoading, movies} = this.state;
+    return (
+      <div className="container">
+        
+        {isLoading ? (
+          <div className = "loader"></div> ) : ( 
+          <div className = "movies">
+            {movies.map(movie => (
+              <Movie 
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.id}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image} 
+                genres = {movie.genres}
+              />
+            ))}
+          </div>         
+          )}
+      </div>
+
+    )
+  }
 }
-
 export default App;
